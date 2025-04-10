f4e282702fdbfe17e73efb5fc01a914b702291ec
bupd <bupdprasanth@gmail.com>
2025-04-11 03:32:42 +0530

    update with sftp
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"io"
 	"log"
-	"time"
+	"os"
 
+	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 )
 
 func main() {
+	// SSH config
 	config := &ssh.ClientConfig{
 		User: "sftp-user1",
 		Auth: []ssh.AuthMethod{
@@ -16,18 +19,41 @@ func main() {
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
+	// Connect to SSH server
 	client, err := ssh.Dial("tcp", "44.202.50.230:22", config)
 	if err != nil {
 		log.Fatal("Failed to dial: ", err)
 	}
+	defer client.Close()
+	log.Println("SSH connection established")
 
-	log.Println("we have a client")
-	time.Sleep(5 * time.Second)
-
-	session, err := client.NewSession()
+	// Create SFTP client
+	sftpClient, err := sftp.NewClient(client)
 	if err != nil {
-		log.Fatal("Failed to create session: ", err)
+		log.Fatal("Failed to create SFTP client: ", err)
 	}
-	log.Println("we have a session")
-	defer session.Close()
+	defer sftpClient.Close()
+	log.Println("SFTP session established")
+
+	// Open local file
+	localFile, err := os.Open("./README.md") // Change to your local file path
+	if err != nil {
+		log.Fatal("Failed to open local file: ", err)
+	}
+	defer localFile.Close()
+
+	// Create destination file on the remote server
+	remoteFile, err := sftpClient.Create("/uploadedfile.txt") // Adjust the remote path
+	if err != nil {
+		log.Fatal("Failed to create remote file: ", err)
+	}
+	defer remoteFile.Close()
+
+	// Copy local file to remote file
+	bytes, err := io.Copy(remoteFile, localFile)
+	if err != nil {
+		log.Fatal("Failed to copy file: ", err)
+	}
+
+	log.Printf("File transferred successfully (%d bytes)\n", bytes)
 }
